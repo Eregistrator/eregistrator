32a855f7213090e7bdd92f9b2118a4cea15589bc
peter stefan <39226425+peti651155@users.noreply.github.com>
2020-10-15 15:13:21 +0200

    ez a második

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'elso teszt modositas'),
+      home: MyHomePage(title: 'ez a második módosítás'),
     );
   }
 }
